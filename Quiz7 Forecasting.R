8db0360454cf838d2d6f3f8fb6e21b6b2f1d6251
haskinsm <60662825+haskinsm@users.noreply.github.com>
2020-11-16 17:22:08 +0000

    Quiz7 Started

--- Quiz7 Forecasting.R
+++ Quiz7 Forecasting.R
@@ -10,4 +10,4 @@ plot(forecast(arima.sim(n=63, list(ar = c(0.8897)), sd = sqrt(0.1796)), h=100))
 plot(forecast(Arima(dowjones, order = c (1,0,0)), h = 1000)) ##Converging to a limit, shown in OneNote notes
 
 plot(forecast(Arima(beer, order = c (1,0,0)), h = 1000)) ##Will always converge -> this behaviour is because of the Ar 1 model => proved this in notes
-##All AR models of order 1, 2nd number in list is the integerated, 3rd number is the moving average, both not covered yet
+##All AR models of order 1, 2nd number in list is the integerated, 3rd number is the moving average, both not covered yet.
