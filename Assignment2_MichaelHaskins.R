4c2f11925ab2ae7a2466a91fc8c0e8ec3d10ceda
haskinsm <60662825+haskinsm@users.noreply.github.com>
2021-02-12 16:35:10 +0000

    LAB 2 Statistical Analysis

---
+++ Assignment2_MichaelHaskins.R
@@ -0,0 +1,62 @@
+
+############################ PART 1 ##############################
+
+EUData = read.csv(file = "Lab 2 - EU_20190102.txt", header = TRUE)
+?read.table
+USData = read.csv(file = "Lab 2 - US_2019_02_01.txt", header = TRUE, sep = ";")
+
+EU_cust_30_50 = subset( EUData, (age >= 30) & (age <= 50))
+write.table(EU_cust_30_50, file = "EU_cust_30_50.csv", row.names = FALSE, sep = ",")
+
+EU_cust_ter_se_enter = subset( EUData, (education == "tertiary") & ((job == "self-employed") | (job == "entrepreneur")))
+write.table(EU_cust_ter_se_enter, file = "EU_cust_ter_se_enter.csv", row.names = FALSE, sep = ",") ##Files stored in WD folder
+?write.table
+
+
+############################## PART 2 ################################
+?levels
+levels( EUData[ ,2])
+for( occupation in levels( EUData[ ,2])){  ## occupation will be admin. on first iteration then blue-collar and so on
+ # print(occupation)
+  DataFrame = subset(EUData, job == occupation)
+  if( occupation == "admin."){
+    ?substr
+    # substr("admin.", 0, 5) # Results in admin
+    write.table(DataFrame, file = paste(  substr("admin.", 0, 5) , ".csv", sep = ""))
+  } else {
+    write.table(DataFrame, file = paste(occupation, ".csv", sep = ""))
+  }
+}
+
+############################# PART 3 ################################
+# [Assume the Euro/Dollar conversion rate is 1 US Dollar = 0.87  Euro]
+EUAmmended = EUData
+USAmmended = USData
+
+dim(EUData)
+dim(USData)
+
+levels(EUAmmended[,9])[1] <- "teleph"  ## Changes all cellular entries to teleph 
+levels(EUAmmended[,9])
+levels(EUAmmended[,9])[2] <- "teleph"  ## Changes all telephone entries to telph
+
+?data.frame
+?sapply
+
+## Changing the US Dollars Balance to Euro
+USAmmended$balance <- USAmmended$balance * 0.87 ## This updates the balance field
+##THis didnt work, only created a vector: sapply(USData$balance, '*', 0.87)
+
+## Removing "poutcome" field from EUAmmended data frame
+EUAmmended$poutcome <- NULL ## This sets the field to null which removes it
+dim(EUAmmended)
+
+## Add Region field with values EU and US
+USAmmended$region <- "US"
+EUAmmended$region <- "EU"
+
+##Now combine the two dataframes using rbind
+USEUData = rbind(USAmmended, EUAmmended)
+
+## Now write new data frame to csv file
+write.table(USEUData, file = "EU_US_Merge.csv", row.names = FALSE, sep = ",")
