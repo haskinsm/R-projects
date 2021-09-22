a8a8bb5bfcb8c395bde4e05940d9132d851fc9ce
haskinsm <60662825+haskinsm@users.noreply.github.com>
2021-09-22 18:03:06 +0100

    Commit of new Data Analytics Module folderr and other miscelanious stuff

--- AnalysisCodeSTAHaskinsm.R
+++ AnalysisCodeSTAHaskinsm.R
@@ -1,10 +1,12 @@
+######## 24/05/21, Michael Haskins, 18323076, 3rd year MSISS
+
 ## Read in already cleaned and manipulated dataset
 dataset = read.csv(file = "cleanProjectData.csv", sep=",", header = TRUE) 
 dim(dataset) 
 # Create a data frame of the dataset
 df = data.frame(dataset)
 dim(df)
-# Change all variables to a factor that are not already factors (except failures and absences)
+# Change all variables to a factor that are not already factors (except failures and absences as they are count variables)
 df$Pedu <- factor(df$Pedu)
 df$traveltime <- factor(df$traveltime)
 df$studytime <- factor(df$studytime)
@@ -16,25 +18,48 @@ df$health <- factor(df$health)
 # Order the FinalGrade field in the order Low-Grade, Average-Grade, High-Grade
 df$FinalGrade <- ordered(df$FinalGrade, levels = c("Low-Grade", "Average-Grade", "High-Grade"))
 
+
+
+######################### Code used for exploratory analysis section ######################
+
+###### Basic breakdown of FinalGrade by Pedu (Parent's Education Level)
 table(dataset$Pedu, dataset$FinalGrade)
 
 
 library(ggplot2)
 
-## Will now createa graph to breakdown the final grade by the level of parent's education
+###### Will now create a graph to breakdown the final grade by the level of parent's education
 gradeGraph <- ggplot(df, aes(x=Pedu, fill = Pedu)) + labs(title="Breakdown of Final Grade by level of Parent's Education", subtitle = "(649 obs)", y="Total count", x="")
 gradeGraph <- gradeGraph + geom_bar()  ## Specify the plot as being a Barchart
-gradeGraph <- gradeGraph + facet_wrap("FinalGrade") ## This will create bands of final grade. i.e. a band for Low-Grade, then one for Average-Grade etc split up by the Pedu variable
-gradeGraph <- gradeGraph + scale_fill_manual(values = c("#00AFBB", "#E7B800", "#FC4E07")) ## Add colour to the Pedu classifications 
+gradeGraph <- gradeGraph + facet_wrap("FinalGrade") ## This will create bands of final grade. i.e. a band for Low-Grade, then one for Average-Grade etc split up by the Pedu levels/groups
+gradeGraph <- gradeGraph + scale_fill_manual(values = c("#00AFBB", "#E7B800", "#FC4E07")) ## Add colour to the Pedu classifications. Will try keep groups colour constant throughout report
 gradeGraph <- gradeGraph + theme(legend.position = "right") ## Place legend to the right 
-gradeGraph <- gradeGraph + geom_text(stat='count', aes(label = ..count..), vjust = 0) # Add count labels above each column
+gradeGraph <- gradeGraph + geom_text(stat='count', aes(label = ..count..), vjust = 0) # Add count labels above each column to esnure easy interpretation 
 gradeGraph <- gradeGraph + scale_x_discrete(labels = NULL, breaks = NULL) + scale_y_continuous(labels = NULL, breaks = NULL, limits = c(0,148)) 
-# Above line: Removes all axis labels and breaks, and alter the y axis limits to increase visiblity.
+# Above line: Removes all axis labels and breaks, and alter the y axis limits to increase visiblity for reader.
 plot(gradeGraph) #Plot the created ggplot barchart
 
 
-###################################### Correlation heatmap ##################################################
-set.seed(123)
+###### Will now create a graph to breakdown the final grade by school
+schoolGraph <- ggplot(df, aes(x=school, fill = school)) + labs(title="Breakdown of Final Grade by school", subtitle = "(649 obs)", y="Total count", x="")
+schoolGraph <- schoolGraph + geom_bar()  ## Specify the plot as being a Barchart
+schoolGraph <- schoolGraph + facet_wrap("FinalGrade") ## This will create bands of final grade. i.e. a band for Low-Grade, then one for Average-Grade etc split up by the School 
+schoolGraph <- schoolGraph + scale_fill_manual(values = c("darkviolet", "yellow")) ## Add colour to the School classifications. Will try keep groups colour constant throughout report
+schoolGraph <- schoolGraph + theme(legend.position = "right") ## Place legend to the right 
+schoolGraph <- schoolGraph + geom_text(stat='count', aes(label = ..count..), vjust = 0) # Add count labels above each column to esnure easy interpretation 
+schoolGraph <- schoolGraph + scale_x_discrete(labels = NULL, breaks = NULL) + scale_y_continuous(labels = NULL, breaks = NULL, limits = c(0,250)) 
+# Above line: Removes all axis labels and breaks, and alter the y axis limits to increase visiblity for reader.
+plot(schoolGraph) #Plot the created ggplot barchart
+
+
+##### Breakdown of Pedu by school
+table(df$school, df$Pedu)
+
+
+
+################### Correlation heatmap 
+
+set.seed(123) # To ensure reproducability
 # Src #https://stackoverflow.com/questions/52554336/plot-the-equivalent-of-correlation-matrix-for-factors-categorical-data-and-mi
 # This code was used in my eploratory analysis
 
@@ -42,7 +67,9 @@ library(tidyverse)
 # install.packages("lsr")
 library(lsr)
 library(dplyr)
+
 #### Make sure MASS project is not loaded (As select function overlaps with desired dplyr select) #############################################################
+detach("package:MASS", unload=TRUE) ## To ensure the select function of MASS does not override select function of dplyr
 
 # function to get chi square p value and Cramers V
 f = function(x,y) {
@@ -69,30 +96,40 @@ dfRes %>%
   labs(title="Correlation Heatmap of School Performance dataset", subtitle = "Calculated using Cramers V (indicates how strongly categorical variables are associated)", 
        y="Variables", x="Variables", caption = "Cramers V ranges from 0 (low corr) to 1 (high corr)")
 
+######################################### End of Exploratory Analysis ##############################
 
-########################################## Ordinal Logistic Regression ##############################
-set.seed(123) ## Set seed to ensure reproducability. 
-########## Need to change ordinal variable FinalGrade from 'Low-Grade', 'Average-Grade' and 'High-Grade' to 1, 2, 3
-dfR = df#data.frame(dataset)
+
+
+
+######################################### Prediction Models ########################################
+
+########################################## Ordinal Logistic Regression ##############
+
+########## Need to change ordinal variable FinalGrade from 'Low-Grade', 'Average-Grade' and 'High-Grade' to 1, 2, 3 (Still an ordinal variable)
+dfR = df # Create new df for this 
 levels(dfR$FinalGrade)[levels(dfR$FinalGrade) == 'Low-Grade'] <- '1'
 levels(dfR$FinalGrade)[levels(dfR$FinalGrade) == 'Average-Grade'] <- '2'
 levels(dfR$FinalGrade)[levels(dfR$FinalGrade) == 'High-Grade'] <- '3'
 
-# src: https://stats.idre.ucla.edu/r/dae/ordinal-logistic-regression/
-#install.packages("MASS")
-library(MASS)
 
-# Randomly split data into 70% training, 30% test
+# src: https://stats.idre.ucla.edu/r/dae/ordinal-logistic-regression/
+
+#install.packages("MASS")
+library(MASS) #Reload MASS package
+
+######## Randomly split data into 70% training, 30% test
+set.seed(123) ## Set seed to ensure reproducability.
 ind <- sample(2, nrow(dfR), replace = TRUE, prob=c(0.7, 0.3))
 trainDf <- dfR[ind==1, ]
 testDf <- dfR[ind==2, ]
-## Data is now split. Since the data is split based on probability, may not have exactly a 70/30 split
+## Data is now split. Since the data is split based on probability, may not have exactly a 70/30 split.
 
 
 # Create Ordinal Logistic Regression model 
 m <- polr(FinalGrade ~ . , data= trainDf, Hess = TRUE)
+# create a model using all variables as explanatory variables and use the training dataset to train the model
 # specify Hess=TRUE to have the model return the observed information matrix from 
-# optimization (called the Hessian) which is used to get standard errors.
+# optimization (called the Hessian) which is used to get standard errors and is required for the summary function.
 
 # Now get summary of the model
 summary(m)
@@ -100,6 +137,7 @@ summary(m)
 # The intercepts indicate where the latent variable is cut to make the three groups that we observe in our data.
 # Note that this latent variable is continuous. In general, these are not used in the interpretation of the results. 
 # The cutpoints are closely related to thresholds, which are reported by other statistical packages
+# Can see the AIC = 791.671
 
 
 ##### Get P-Values
@@ -117,11 +155,11 @@ ci <- confint(m) # default method gives profiled CIs -> obtained by profiling th
 ci 
 # If the 95% CI does not cross 0, the parameter estimate is statistically significant.
 # The estimates in the output are given in units of ordered logits, or ordered log odds
-## Better to use above ci than below line as cannot assume normailty for all vars. 
+## Better to use above ci than below (commented out) line as cannot assume normailty for all vars. 
 # confint.default(m) ## Not preferred: CIs assuming normality -> obtained by using the standard errors and assuming a normal distribution.
 
 ## For ease of interpretation will convert the coefficinets into odds ratios 
-# To get the OR and confidence intervals, we just exponentiate the estimates and confidence intervals.
+# To get the OR and confidence intervals, exponentiate the estimates and confidence intervals.
 ###### odds ratios
 exp(coef(m))
 ## OR and CI
@@ -173,23 +211,145 @@ s ## If the assumption is to stand for an explanatory variable the readings in '
 
 # In the below graph If the proportional odds assumption holds, for each predictor variable, distance between the symbols for
 # each set of categories of the dependent variable, should remain similar.
-plot(s, which=1:3, pch=1:3, main="", xlab='logit', xlim=c(-3,0), ann = FALSE) #xlim=range(s[,3:4]))
+plot(s, which=1:3, pch=1:3, main="", cex.lab=0.7, xlab='logit', xlim=c(-3,0), ann = FALSE) # yaxt="n" -> No y axis labels
 title(main='Verification of the parallel slopes assumption (of Ordinal Regr.)', 
       sub='The assumption holds if for each predictor \n variable the distance between the symbols \n for each set of categories is similar.',
       cex.sub = 0.75, font.sub = 4, adj=1) #adj=1 stes the subtitle to be algned as far right as possible
+
 # which =1:3 as decision var has 3 levels
 # Set x range as being between -3 to 0 to ensure graph was close up
 # I think I will have to remove category labels of individual variables to increase readability
 # Will add subtitle to ensure graph is standalone 
+# Cant make y axis labels appear any nicer unfortunately. 
 
 
-##################### First make predictions:
-# predict some probabilities from my model 
-myPredict = predict(m, type = "response")
+##################### Now obtain the predicted probabilities:
+############################################### Abandoned Block of code ############################
+### Create new data frame contain all possible combinations
+#newdat <- data.frame(
+  #school = rep(1:2, 1600), # 1="GP", 2="MS"
+  #sex = rep(1:2, 1600), #1='M;, 2='F'
+  #Pstatus = rep(1:2, 1600), # "A"=1, "T"=2
+  #traveltime = rep(1:5, 640),
+  #studytime = rep(1:5, 640),
+  #failures = rep(1:3, length.out = 3200),
+  #schoolsup = rep(1:2, 1600), #1=Yes, 2=No
+  #famsup = rep(1:2, 1600),#1=Yes, 2=No
+  #higher = rep(1:2, 1600),#1=Yes, 2=No
+  #internet = rep(1:2, 1600),#1=Yes, 2=No
+  #Dalc = rep(1:5, 640),
+  #Walc = rep(1:5, 640),
+  #health = rep(1:5, 640),
+  #absences = rep(1:32, 100), #3200 rows
+  #Pedu= rep(1:3, length.out = 3200)
+#)
+#newdat <- cbind(newdat, predict(m, newdat, type = "probs"))
+
+
+#install.packages("reshape2")
+#library(reshape2)
+
+#lnewdat <- melt(newdat, id.vars = c("school", "sex", "Pstatus", "traveltime", "studytime", "failures", "schoolsup", 
+ #                                   "famsup", "higher", "internet", "Dalc", "Walc", "health", "absences", "Pedu" ),
+  #              variable.name = "Level", value.name="Probability")
+
+#ggplot(lnewdat, aes(x = internet, y = Probability, colour = Level)) +
+ # geom_line() + facet_grid(Pedu ~ higher, labeller="label_both")
+
+######################################### End of abadnoned block #################################
+
+
+############## Predict the (FinalGrade) classification of the test dataset
+predictTest = predict(m, testDf)
+
+#### Compute confusion matrix
+confMatrix = table(testDf$FinalGrade, predictTest)
+## Add row and col names
+confMatrix <- rbind(as.numeric(names(confMatrix)), confMatrix)
+rownames(confMatrix) <- c("Real (rows): Low-Grade", "             Avergae-Grade", "             High-Grade")
+colnames(confMatrix) <- c("Predicted (Cols): Low-Grade", "Avergae-Grade", "High-Grade")
+confMatrix
+
+#### Calc Misclassification %
+correctClassifications = confMatrix[1] + confMatrix[5] + confMatrix[9]
+total = dim(testDf)[1]
+misClassificationPerc = round(((total - correctClassifications)/ total)*100, 2) 
+misClassificationPerc ##  39.29% -> Very high. 
+
+###### Abandoned 
+### Misclassification % for each level
+#numLowGrades = length(testDf$FinalGrade[testDf$FinalGrade == "1"])
+#correctLowGrades = 
+###### End abandoned 
+
+############ Plotting the probability an observation belongs to different bands of FinalGrade based on levels of Pedu and higher
+#install.packages("effects")
+library("effects")
+Effect(focal.predictors = "Pedu",m)
+plot(Effect(focal.predictors = "higher",m))
+plot(Effect(focal.predictors = "Pedu",m))
+plot(Effect(focal.predictors = c("Pedu", "higher"),m), main = "Effect of the levels of Pedu and higher on FinalGrade outcome")
+# Can draw a number of interesting insights . 
+
+
+############################## VUS: Volume Under the ROC Surface (Suitable for Ordinal Logistic regression models)
+#install.packages("VUROCS")
+library("VUROCS")
+?VUS
+# This function computes the volume under the ROC surface (VUS) for a vector of 
+# realisations y (i.e. realised categories) and a vector of predictions fx (i.e. values of the a ranking function f) 
+volumeUnderVUS = VUS(testDf$FinalGrade, predictTest)$val
+volumeUnderVUS # is 0.1102031. This is very low nd indicates the model is not very good at predicting the finalGrade of students.
+
+## There is no point in plotting this as it would be a 3D ROC curve, which is very hard to interpret for readers. 
+## I also have not been able to find a package for plotting a 3D ROC curve that I can get to work.
+
+
+
+## Some prediction probabilities
+myPredict = predict(m, type = "probs", newdata = testDf)
 myPredict
+# If I have time I might write a double for loop to ascertain the best thresholds, but not sure if this will be useful or worthwhile 
 
-# install.packages("ROCR")
-library(ROCR)
+
+
+
+
+############################################### Try Model with only variables that were deemed to be statsitically signifcant from CIs##########################
+ci 
+# If the 95% CI does not cross 0, the parameter estimate is statistically significant.
+## All of the below variables are deemed to be statistically signifcant, except Pedu, but as it was very close to be 
+# statistically signifcant I included it. 
+m2 <- polr(FinalGrade ~ school + higher + absences + Pedu + failures , data= trainDf, Hess = TRUE)
+m2
+summary(m2) ## AIC is 798, which is higher than model 'm', which has an AIC of 791.671 so this model is worse at modelling the original data
+#summary(m)
+############## Predict the (FinalGrade) classification of the test dataset using model 2
+predictTestm2 = predict(m2, testDf)
+
+#### Compute confusion matrix
+confMatrixm2 = table(testDf$FinalGrade, predictTestm2)
+confMatrixm2
+
+#### Calc Misclassification %
+correctClassificationsm2 = confMatrixm2[1] + confMatrixm2[5] + confMatrixm2[9]
+total = dim(testDf)[1]
+misClassificationPercm2 = round(((total - correctClassificationsm2)/ total)*100, 2) 
+misClassificationPercm2 ##  43.37% -> Very high.Higher than model m, so this model is worse at predicting the outcome of FinalGrade  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Data Analytics 4thYr/ClassificationTree.R
@@ -0,0 +1,62 @@
+### Decision (Classification) Trees ###################
+# 22/09/2021
+
+########## Read in Data ##############
+##setwd("~/TCD/TCD 2021-22/STU44003") ##Presume this is code for him
+getwd() ##Check directory
+Data <- read.csv("Data Analytics 4thYr/Data/DT-Credit.csv", header=TRUE, sep= ";") ##Saved in a folder called 'Data'in this files directory
+
+
+######### Check and manipulate dataset########
+str(Data)  # str() is used for compactly displaying the internal structure of a R object
+Data <- Data[,-1] # Remove 1st column of data
+str(Data) 
+attach(Data) #used to access the variables present in the data framework without calling the data frame.
+
+
+########## Buidling Decision Tree Model #################
+#install.packages("rpart")
+library(rpart)
+## NOTE: INCORRECT as variables entered in wrong. Need to specify what variables are factors and NOT continuous, otherwise R will assume everything is continuous.****************************8
+## Fix is below. 
+DT_Model <-rpart(RESPONSE~., data=Data, control=rpart.control(minsplit=60, minbucket=30, maxdepth=4 )) 
+# RESPONSE is Var in Data its the response var, ~. uses all other vars in Data as explanatory vars 
+# minsplit: the minimum number of observations that must exist in a node for a new split. 
+# minbucket: the minimum number of observations in any terminal node -> terminal node is the end node I think (the bucket) -> Tells you the classification decison or prob distribution for classification 
+# Maxdepth: Maximum depth for any node, with the root node counted as depth 0.  
+DT_Model
+mean(RESPONSE)
+
+########## Visualization of Decision Tree ############
+#install.packages ("partykit") 
+library("partykit") 
+plot(as.party(DT_Model)) 
+# Can see chl_account was the first explantory var picked for model
+## Note R did not realize that some vars were factors and not continuous variables, not numerical ***********************************
+## It also did not realize the response var was not continuous but binary. 
+
+
+################## Now fix Data, specifying what is factors 
+str(Data) 
+cols <- c(1, 3:9, 11:21, 23:31)
+Data[cols] <- lapply(Data[cols],factor)  ##lapply applys function to everything in cols -> here it turns everything into factors 
+str(Data)
+
+
+######## Now create a Decison Tree model using Correctly altered Data ######### 
+DT_Model <-rpart(RESPONSE~., data=Data, control=rpart.control(minsplit=60, minbucket=30, maxdepth=4 )) 
+print(DT_Model)
+plot(as.party(DT_Model)) 
+### Can now see its much easier to navigate the tree and makes much more sense (Dark grey is yes, can see percentages of response var (People who paid back the loan who got it)) 
+
+
+####### Change the response to Y/N answers and rerun the decision tree model function. ##############
+Target=ifelse(RESPONSE==1,'Y','N')
+Data <- data.frame(Data,Target)
+str(Data)
+Data1=Data[,-31] 
+DT_Model1<-rpart(Target~., data=Data1, control=rpart.control(minsplit=60, minbucket=30, maxdepth=4 ))
+plot(as.party(DT_Model1))
+print(DT_Model1)
+
+# Change the control parameters and see the change in the output (minsplit=60, minbucket=30, maxdepth=8). 
